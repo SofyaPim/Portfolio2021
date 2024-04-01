8ad48dec5bd00fe3ecac47795ff1acf743ad6b7b
Sofya <48826470+SofyaPim@users.noreply.github.com>
2024-04-01 11:09:43 +0300

    Add files via upload

--- js/myWorks.js
+++ js/myWorks.js
@@ -1,25 +1,27 @@
-const gallery = [{
-        link: "https://sofyapim.github.io/Loopstudios-landing-page/",
-        photo: "images/Loopstudios.webp",
-        alt: "Loopstudios"
-
-
-    },
-    {
-        link: "https://sofyapim.github.io/frontend-mentor-easybank/",
-        photo: "images/easybank.webp",
-        alt: "easybank"
-
-
-    },
-    {
-        link: "https://sofyapim.github.io/VolgaFood-webpack/dist/index.html",
-        photo: "images/VolgaFood.webp",
-        alt: "VolgaFood"
-
-
-    }
-
+const gallery = [
+  {
+    link: "https://sofyapim.github.io/Loopstudios-landing-page/",
+    photo: "images/Loopstudios.webp",
+    alt: "Loopstudios",
+  },
+  {
+    link: "https://sofyapim.github.io/frontend-mentor-easybank/",
+    photo: "images/easybank.webp",
+    alt: "easybank",
+    head: "FrontendMentor Challenge",
+  },
+  {
+    link: "https://sofyapim.github.io/VolgaFood-webpack/dist/index.html",
+    photo: "images/VolgaFood.webp",
+    alt: "VolgaFood",
+    head: "Готовая еда",
+  },
+  {
+    link: "https://sofyapim.github.io/bootstrap_module/index.html",
+    photo: "images/bootstrap_module.jpg",
+    alt: "bootstrap_module",
+    head: "Bootstrap модуль",
+  },
 ];
 
 function cardWorks(card) {
@@ -27,13 +29,16 @@ function cardWorks(card) {
 
 <div class="card">
 
-<a target="_blank" href="${card.link}"
-    rel="nofollow">
-    <img src="${card.photo}" alt="${card.alt}">
-</a>
+    <h5>${card.head}</h5>
+        <a target="_blank" href="${card.link}"
+            rel="nofollow">
+            <img src="${card.photo}" alt="${card.alt}">
+        </a>
+
+
 </div>
        
- `
+ `;
 
 };
 
